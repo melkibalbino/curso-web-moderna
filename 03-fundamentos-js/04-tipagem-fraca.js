70f6e541f154aeb05067629702803667a1f44331
melkibalbino <melkibalbinoads@gmail.com>
2021-07-02 20:51:57 -0300

    tipagem fraca

---
+++ 03-fundamentos-js/04-tipagem-fraca.js
@@ -0,0 +1,15 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Javascript tem a tipagem fraca
+// o tipo existe porem, pode ser mudado passando valores com tipo diferentes
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar Criar Variaveis com nome genericos no dia dia
+// Ex:
+let valor = ''
+let numero = 1
+let pqp = false // Produto Quimico Perigoso... KKKKKK
