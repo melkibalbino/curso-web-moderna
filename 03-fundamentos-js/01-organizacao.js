b90955b7709e6197fd118d9aa8fa2d5e0429afd4
melkibalbino <melkibalbinoads@gmail.com>
2021-07-02 20:05:28 -0300

    Add file 01-organizacao.js

---
+++ 03-fundamentos-js/01-organizacao.js
@@ -0,0 +1,12 @@
+console.log(
+  'Setença de codigo de Código') // Setença de codigo
+// pode ter variaas linhas
+
+{
+  //Bloco de codigo
+  {
+    // Bloco aninhado
+    console.log("Olá") // string com aspas duplas e sentença com ';'
+  }
+  console.log('Mundo') // string com aspas simples e sem ';'
+}
