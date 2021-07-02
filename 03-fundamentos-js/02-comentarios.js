c294398a19cb405cd0a1566b46eef61ad964370b
melkibalbino <melkibalbinoads@gmail.com>
2021-07-02 20:43:25 -0300

    variaveis e comentarios

---
+++ 03-fundamentos-js/02-comentarios.js
@@ -0,0 +1,8 @@
+// Comentario de uma linha
+console.log('Linha 1') // outro uso de comentario de uma linha
+
+/*
+  Comentario de
+  multiplas linhas
+*/
+console.log('Linha 2')
---
+++ 03-fundamentos-js/03-variaveis-e-constantes.js
@@ -0,0 +1,19 @@
+var a = 3 // Atribuição do valor 3 a variavel a
+// Evitar usar var: e recomendado usar let
+let b = 4
+
+var a = 30
+b = 40
+// let b = 40 // Geraria um erro
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50 // Retorna erro pois nao pode ser mudada
+
+console.log(c)
