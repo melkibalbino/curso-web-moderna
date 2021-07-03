0ab1f1a2b36796a157f23cbf2993775c9a925af1
melkibalbino <melkibalbinoads@gmail.com>
2021-07-03 03:44:05 -0300

    alguns cuidados com numeros

---
+++ 03-fundamentos-js/06-numeros-alguns-cuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) // retorna tipo infinito
+console.log('10' / 2) // faz a divisão so em caso de valor inteiro na string
+console.log('show' * 2) // Não repete a string
+console.log(0.1 + 0.7) // não retorna 0.8
+// console.log(10.toString())
+// console.log((10).toString())
+console.log((10.1564598).toFixed(2))
