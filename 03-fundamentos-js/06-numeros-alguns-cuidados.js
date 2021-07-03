0fe3fe06a010edfc98f037b5b3d32a2f9864186e
melkibalbino <melkibalbinoads@gmail.com>
2021-07-03 14:44:52 -0300

    Errata alguns cuidados com number

--- 03-fundamentos-js/06-numeros-alguns-cuidados.js
+++ 03-fundamentos-js/06-numeros-alguns-cuidados.js
@@ -1,7 +1,20 @@
 console.log(7 / 0) // retorna tipo infinito
-console.log('10' / 2) // faz a divisão so em caso de valor inteiro na string
 console.log('show' * 2) // Não repete a string
 console.log(0.1 + 0.7) // não retorna 0.8
 // console.log(10.toString())
 // console.log((10).toString())
 console.log((10.1564598).toFixed(2))
+
+/**
+ * Quando dois tipo diferentes entao na mesma operação
+ * Como por exempro uma string + number
+ * o js tenta primeiro tratar os dois valores como string
+ * no caso do + ele concatena, porem o sinal de divisor
+ * não faz sentindo em opeção com strings então ele transforma
+ * em number e faz a operação
+ */
+
+console.log('10' / 2) // faz a divisão so em caso de valor inteiro na string
+const vl = '3' + 2
+console.log(vl) // retorna uma string
+console.log(typeof vl) // retorna uma string
